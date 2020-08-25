e5f187b76f1cb3d3006ba45414d8b0e82e07207f
seohuibae <seohuibae73@gmail.com>
2020-08-25 11:39:34 +0900

    upload qualitative

--- qualitative.py
+++ qualitative.py
@@ -1,17 +1,40 @@
 import numpy as np 
+import pandas as pd 
 import jamotools 
+from scipy.spatial import distance as dis
 
-
-def qualitative(argtext, arguni):
-    standards = np.genfromtxt('./data_qualitative/학생종합채점표.xlsx', delimiter=',')
+def qualitative(text, uni):
+    f = open('./data_qualitative/standard.txt', 'r')
+    dfstandard = pd.read_csv(f, delimiter='\t')
     v = jamotools.Vectorizationer(rule=jamotools.rules.RULE_1, max_length=None, prefix_padding_size=0)
-    text = v.vectorize(argtext)
+    vectext = v.vectorize(text)
+
     category = ['학업역량', '전공적합성', '인성', '발전가능성']
+    scores = []
     for c in category: 
-        print(standards[c])
+        question = ''
+        df = dfstandard.loc[dfstandard.index[dfstandard.iloc[:,0]==c].tolist()].iloc[:,2]
+        for i,value in df.iteritems():
+            question += value+'\n'
+        vecques = v.vectorize(question)
+        ml = max(len(vectext), len(vecques))
+        vectext = np.concatenate((vectext, np.zeros(ml-len(vectext))))
+        vecques = np.concatenate((vecques, np.zeros(ml-len(vecques))))
+      
+        score = 1 - dis.cosine(vectext, vecques)
+        score = score * 40
+        if score >= 5.0: score = 5.0
+        # score = dis.euclidean(vectext, vecques)
+        scores.append(score)
+
+    # TODO fetch evaluation from DB 
+    evaluation = 'NULL'
+    scores = dict(zip(category, scores))
+    return scores, evaluation
 
 
 
 if __name__ == '__main__':
-    text = np.loadtxt('./data_qualitative/서가연_양지고등학교_여.txt')
-    qualitative(text, '서울대학교')
+    f = open('./data_qualitative/sample_06.txt','r')
+    text = f.read()
+    print(qualitative(text, '서울대학교'))
